6b5ec85234534872bf95e2cee97b06339a83040c
Hannah Warmbier <hannahwarmbier@gmail.com>
2015-12-30 18:28:45 -0600

    Add app/, move code from public/index.php...
    ...to app/bootstrap.php. Also fix namespace.

---
+++ app/bootstrap.php
@@ -0,0 +1,28 @@
+<?php
+
+use Spark\Skeleton\Domain;
+
+Spark\Application::build()
+->setConfiguration([
+    Spark\Configuration\AurynConfiguration::class,
+    Spark\Configuration\DiactorosConfiguration::class,
+    Spark\Configuration\NegotiationConfiguration::class,
+    Spark\Configuration\PayloadConfiguration::class,
+    Spark\Configuration\RelayConfiguration::class,
+    Spark\Configuration\WhoopsConfiguration::class,
+])
+->setMiddleware([
+    Relay\Middleware\ResponseSender::class,
+    Spark\Handler\ExceptionHandler::class,
+    Spark\Handler\DispatchHandler::class,
+    Spark\Handler\JsonContentHandler::class,
+    Spark\Handler\FormContentHandler::class,
+    Spark\Handler\ActionHandler::class,
+])
+->setRouting(function (Spark\Directory $directory) {
+    return $directory
+    ->get('/hello[/{name}]', Domain\Hello::class)
+    ->post('/hello[/{name}]', Domain\Hello::class)
+    ; // End of routing
+})
+->run();
--- public/index.php
+++ public/index.php
@@ -1,31 +1,4 @@
 <?php
 
-// Include Composer autoloader
-require __DIR__ . '/../vendor/autoload.php';
-
-use Spark\Project\Domain;
-
-Spark\Application::build()
-->setConfiguration([
-    Spark\Configuration\AurynConfiguration::class,
-    Spark\Configuration\DiactorosConfiguration::class,
-    Spark\Configuration\NegotiationConfiguration::class,
-    Spark\Configuration\PayloadConfiguration::class,
-    Spark\Configuration\RelayConfiguration::class,
-    Spark\Configuration\WhoopsConfiguration::class,
-])
-->setMiddleware([
-    Relay\Middleware\ResponseSender::class,
-    Spark\Handler\ExceptionHandler::class,
-    Spark\Handler\DispatchHandler::class,
-    Spark\Handler\JsonContentHandler::class,
-    Spark\Handler\FormContentHandler::class,
-    Spark\Handler\ActionHandler::class,
-])
-->setRouting(function (Spark\Directory $directory) {
-    return $directory
-    ->get('/hello[/{name}]', Domain\Hello::class)
-    ->post('/hello[/{name}]', Domain\Hello::class)
-    ; // End of routing
-})
-->run();
+require dirname(__DIR__) . '/vendor/autoload.php';
+require dirname(__DIR__) . '/app/bootstrap.php';
