df67f6fb1a938bd6dcd9fb2079c4ca17562781b8
Hannah Warmbier <hannahwarmbier@gmail.com>
2015-12-30 19:55:38 -0600

    Update app/*.php config files
    - Add PlatesResponderConfiguration to config.php
    - Add di.php to instantiate and prepare Injector prior to configuration
    - Add injector to bootstrap.php via di.php

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,9 +1,10 @@
 <?php
 
+use Auryn\Injector;
 use Spark\Application;
 
-Application::build()
-->setConfiguration(require __DIR__ . '/config.php')
-->setMiddleware(require __DIR__ . '/middleware.php')
-->setRouting(require __DIR__ . '/routes.php')
-->run();
+Application::build(require __DIR__ . '/di.php')
+    ->setConfiguration(require __DIR__ . '/config.php')
+    ->setMiddleware(require __DIR__ . '/middleware.php')
+    ->setRouting(require __DIR__ . '/routes.php')
+    ->run();
