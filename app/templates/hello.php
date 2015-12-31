b457aba8ac898241697e8d7adb81b8b971d3fa99
Hannah Warmbier <hannahwarmbier@gmail.com>
2015-12-30 19:52:47 -0600

    Add app/templates and public/css...
    - in templates: master|hello.php
    - in css: style.css

---
+++ app/templates/hello.php
@@ -0,0 +1,6 @@
+<?= $this->layout('master') ?>
+<?php $this->start('body') ?>
+<h1>Spark</h1>
+<p>Hello, <?=$name?>!</p>
+
+<?php $this->stop() ?>
---
+++ app/templates/master.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Spark Skeleton</title>
+<meta charset="utf-8">
+<link href="layout/favicon.gif" rel="shortcut icon" />
+<link rel="stylesheet" href="/css/style.css" type="text/css" media="screen" />
+</head>
+<body>
+<?=$this->section('body')?>
+</body>
+</html>
